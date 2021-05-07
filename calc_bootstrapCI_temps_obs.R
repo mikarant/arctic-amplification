55baff9e6105ef71330e3771903a560bb5589856
Otto Hyvärinen <otto.hyvarinen@fmi.fi>
2021-05-07 15:49:23 +0300

    Add two time periods. And parallel processing in "boot".

--- calc_bootstrapCI_temps_obs.R
+++ calc_bootstrapCI_temps_obs.R
@@ -1,3 +1,6 @@
+# Do bootstrap estimates for two time periods,
+# 1979-2018 and 1980-2019
+
 # I think the boot library is available in the standard R installation,
 # so there is no need to install it separately
 library(boot)
@@ -7,49 +10,79 @@ conf = .9
 
 # The number of bootstrap replicates/samples
 # when testing, use something like this
-# R = 500
+# R = 5000
 # when doing the final results, use something like this
-R = 50000
+R = 150000
 
 arc = read.csv("arctic_temps_obs.csv", header = TRUE)
 ref = read.csv("reference_temps_obs.csv", header = TRUE)
 
-# the first column is the year, other columns are the data for each source
-for (k in 2:ncol(arc)) {
-  
-  print(names(arc)[k])
-  
-  da = data.frame(x = ref$Year,
-                  y.arc = arc[, k],
-                  y.ref = ref[, k])
-
-  b = boot(da, function(a, i) {
-    # here the magic happens: 
-    # i is the vector of indices to our data which define the bootstrap sample
-    # then we fit the linear regression for both arc and ref, and 
-    # save the ratio of slope coefficients
-    coefficients(summary(lm(y ~ x , data.frame(
-      x = a$x[i], y = a$y.arc[i]
-    ))))[2, 1] /
-      coefficients(summary(lm(y ~ x , data.frame(
-        x = a$x[i], y = a$y.ref[i]
-      ))))[2, 1]
-  }, R = R)
-  
-  bci = boot.ci(b, conf = conf, type = c("perc", "bca"))
-  
-  r = c(names(arc)[k], b$t0, bci$percent[4:5], bci$bca[4:5] )
-  if(k==2){
-    res = r
-  }else{
-    res = rbind(res,r)    
-  }
-  
+# A sanity check
+if(! all(arc$Year == ref$Year)){
+  cat("WARNING! Something wrong with the years in the observations??\n")
 }
 
-# it is easier to manipulate column names etc. using the data frame class
-res = as.data.frame(res)
+for (mycase in 1:2) {
+  
+  if(mycase == 1){
+    start.year = 1979
+    end.year = 2018
+  }
+  if(mycase == 2){
+    start.year = 1980
+    end.year = 2019
+  }
 
-colnames(res) = c("data","ratio","CIlowerPercentile","CIupperPercentile","CIlowerBCa","CIupperBCa")
+  # the first column is the year, other columns are the data for each source
+  for (k in 2:ncol(arc)) {
+    cat(mycase, names(arc)[k], "\n")
 
-write.csv(res,"bootstrapCI_temps_obs.csv", row.names = FALSE, quote = FALSE)
+    i = ref$Year >= start.year & ref$Year <= end.year
+    
+    da = data.frame(x = ref$Year[i],
+                    y.arc = arc[i, k],
+                    y.ref = ref[i, k])
+    
+    b = boot(da, function(a, i) {
+      # here the magic happens:
+      # i is the vector of indices to our data which define the bootstrap sample
+      # then we fit the linear regression for both arc and ref, and
+      # save the ratio of slope coefficients
+      coefficients(summary(lm(
+        y ~ x , data.frame(x = a$x[i], y = a$y.arc[i])
+      )))[2, 1] /
+        coefficients(summary(lm(
+          y ~ x , data.frame(x = a$x[i], y = a$y.ref[i])
+        )))[2, 1]
+    }, R = R, parallel = "multicore", ncpus = 4)
+    
+    bci = boot.ci(b, conf = conf, type = c("perc", "bca"))
+    
+    r = c(names(arc)[k], b$t0, bci$percent[4:5], bci$bca[4:5])
+    if (k == 2) {
+      res = r
+    } else{
+      res = rbind(res, r)
+    }
+    
+  }
+  
+  # it is easier to manipulate column names etc. using the data frame class
+  res = as.data.frame(res)
+  
+  colnames(res) = c(
+    "data",
+    "ratio",
+    "CIlowerPercentile",
+    "CIupperPercentile",
+    "CIlowerBCa",
+    "CIupperBCa"
+  )
+  
+  write.csv(
+    res,
+    sprintf("bootstrapCI_temps_obs_%d%d.csv", start.year, end.year),
+    row.names = FALSE,
+    quote = FALSE
+  )
+}
