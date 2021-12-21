e4ec0d23b5a502aa878ab517ddff82c2cec97855
mikarant <mika.rantanen@fmi.fi>
2021-12-21 10:27:32 +0200

    changed paths

--- aa_maps_for_supplementary.py
+++ aa_maps_for_supplementary.py
@@ -11,7 +11,6 @@ import xarray as xr
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
 import numpy as np
-from copy import copy
 from matplotlib import cm  
 from matplotlib.colors import ListedColormap  
 from cartopy.util import add_cyclic_point
@@ -19,11 +18,8 @@ from cartopy.util import add_cyclic_point
 
 def plotMap():
 
-    #import cartopy.feature as cfeature
-
     #Set the projection information
     proj = ccrs.NorthPolarStereo()
-    # proj = ccrs.LambertConformal(central_longitude=18.0,central_latitude=53, standard_parallels=[53])
     #Create a figure with an axes object on which we will plot. Pass the projection to that axes.
     fig, axarr = plt.subplots(nrows=1, ncols=4, figsize=(15, 6), constrained_layout=False, dpi=200,
                           subplot_kw={'projection': proj})
@@ -39,7 +35,6 @@ def plot_background(ax):
     import matplotlib.path as mpath    
     import matplotlib.ticker as mticker
 
-    # ax.set_extent([40, 0, 54, 70])
     ax.set_extent([-180, 180, 50,90], crs=ccrs.PlateCarree())
     # ax.add_feature(cfeature.LAND.with_scale('110m'),facecolor=cfeature.COLORS['land'])
     # ax.add_feature(cfeature.LAKES.with_scale('50m'),facecolor=cfeature.COLORS['land'],zorder=1,edgecolor='k',linewidth=1.5) 
@@ -57,7 +52,9 @@ def plot_background(ax):
     ax.set_boundary(circle, transform=ax.transAxes)
     return ax
 
-datapath = '/home/rantanem/Documents/python/data/arctic_warming/'
+datapath = '/Users/rantanem/Documents/python/data/arctic_warming/'
+
+# define the datafiles
 
 files = {'gistemp': datapath + 'GISTEMP.nc',
          'best': datapath + 'BEST.nc',
@@ -91,9 +88,11 @@ titles = {'gistemp': 'a) Gistemp',
 
 trendfiles = {}
 
-years = np.arange(1979, 2020, 1)
+
+# calculate AA ratios and store them to "trendfiles"
 
 for f in files:
+    print('Calculating AA ratio for '+ f)
 
     # calculate annual means
     annual_means = cdo.yearmean(input =  "-selyear,1980/2019 " + files[f])
@@ -103,7 +102,6 @@ for f in files:
         
 
     # take the global mean
-    # output = datapath + f + '.nct'
     output = cdo.fldmean(input = trend2)
 
     # open the dataset and pick up the global average trend
@@ -114,13 +112,10 @@ for f in files:
     warming_ratio = cdo.divc(global_mean, input = trend2)
     trendfiles[f] = warming_ratio
     
-    print(global_mean)
-    
 
 
 
-
-# colormap
+# define the colormap
 Reds = cm.get_cmap('Reds', 12)
 Blues = cm.get_cmap('Blues', 10)
 newcolors = Reds(np.linspace(0, 1, 12))
@@ -136,9 +131,6 @@ newcm.set_under(cm.get_cmap('Blues')(0.99), 1.0)
 newcm.set_over(cm.get_cmap('Greys')(0.75), 1.0)
 
 
-# palette = copy(plt.get_cmap('Reds'))
-# palette.set_under('white', 1.0)  # 1.0 represents not transparent
-
 cmin = 0
 cmax=  7
 inc = 0.5
@@ -151,24 +143,22 @@ fig, axlist = plotMap()
 i = 0
 for ff in trendfiles:
     
+    # open the datafile
     ds = xr.open_dataset(trendfiles[ff])
     
     f = ds[variables[ff]]
     
     f_new, new_lon = add_cyclic_point(f, coord=f[lons[ff]])
 
+    # draw filled contours
     f_contourf = axlist[i].contourf(new_lon, f[lats[ff]], f_new.squeeze(), levels=f_levels, zorder=2, extend='both', 
                             cmap=newcm, transform = ccrs.PlateCarree())
-    # f_contour = axlist[i].contour(new_lon, f[lats[ff]], f_new.squeeze(), levels=f_levels, zorder=2, colors='k', 
-    #                         linewidths=0.5, transform = ccrs.PlateCarree())
-    # axlist[i].clabel(f_contour, f_levels, inline=True, fontsize=10, fmt='%1.1f')
 
-
+    # plot titles
     axlist[i].set_title(titles[ff], fontsize=16)
     i += 1
 
-# axlist[-1].set_visible(False)
-   # 
+
 fig.subplots_adjust(right=0.8, top=0.75, wspace=0.1)
 cbar_ax = fig.add_axes([0.15, 0.1, 0.62, 0.05])
 cb = fig.colorbar(f_contourf, orientation='horizontal',pad=0.05,fraction=0.053, cax=cbar_ax)
@@ -178,5 +168,5 @@ labels = np.arange(cmin,cmax+inc,inc*2)
 cb.set_ticks(labels)
 
 
-plt.savefig('/home/rantanem/Documents/python/figures/arctic_aa_supplementary.png',dpi=200,bbox_inches='tight')
+plt.savefig('/Users/rantanem/Documents/python/figures/arctic_aa_supplementary.png',dpi=200,bbox_inches='tight')
 
