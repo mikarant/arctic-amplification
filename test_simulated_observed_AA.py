c6e834e6dff3a15d6127e44057e40e806b814482
mikarant <mika.rantanen@fmi.fi>
2021-12-22 17:27:59 +0200

    cleaning and better documenting

--- test_simulated_observed_AA.py
+++ test_simulated_observed_AA.py
@@ -10,45 +10,53 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+import sys
 
-model='MPI-ESM'
+# Select which model dataset you want to test: CMIP5, CMIP6, MPI-GE or CanESM5
+model='CMIP6'
+
+# select the ending year of the AA ratio
 year = 2018
+
+# Select how many repetitions to use to construct the distributions
+n_times = 10000
 
 plot_years=np.arange(year, year+1)
     
-if model=='MPI-ESM':
+if model=='MPI-GE':
     years = np.arange(1889,2100)
 else:
     years = np.arange(1889,2101)
 ind = np.isin(years, plot_years)
 
+# read modelled AA ratios
+
 if model=='CMIP6':
-    mpi_ds = pd.read_csv('/Users/rantanem/Documents/python/data/arctic_warming/cmip6_aa.csv', index_col=0)
-    canesm_col = [s for s in mpi_ds.columns if 'CanESM5'.rstrip() in s]
-    cmip6_without_canesm = [m for m in mpi_ds.columns if m not in canesm_col]
-    mpi_ds = mpi_ds[cmip6_without_canesm]
-    mpi_ratios_plot=mpi_ds.loc[plot_years].values.transpose()
-    aa_ratios = mpi_ds.loc[plot_years]
-    all_realizations = aa_ratios.columns
-    models = pd.read_excel('/Users/rantanem/Downloads/alexey_models.xlsx')
+    df = pd.read_csv('/Users/rantanem/Documents/python/data/arctic_warming/cmip6_aa.csv', index_col=0)
+    # exlude CanESM5 model from CMIP6
+    canesm_col = [s for s in df.columns if 'CanESM5'.rstrip() in s]
+    cmip6_without_canesm = [m for m in df.columns if m not in canesm_col]
+    df = df[cmip6_without_canesm]
+   
+    aa_ratios = df.loc[plot_years]
+    all_realizations = df.columns
+    models = pd.read_excel('/Users/rantanem/Documents/python/data/arctic_warming/list_of_models.xlsx')
     models = models[models.ssp245>0]
     models = models[models.Name!='CanESM5']
+    models_with_multiple_real = models[models.ssp245>1]
 elif model=='CMIP5':
-    mpi_ds = pd.read_csv('/Users/rantanem/Documents/python/data/arctic_warming/cmip5/cmip5_ratios.csv',index_col=0)
-    mpi_ratios_plot=mpi_ds.loc[plot_years].values.transpose().squeeze()
-elif model=='MPI-ESM':
-    mpi_ds = xr.open_dataset('/Users/rantanem/Documents/python/data/arctic_warming/data_pdf_plots_'+model+'_rcp45.nc')
-    mpi_ratios_plot=mpi_ds.aa.values[:,ind]
+    df = pd.read_csv('/Users/rantanem/Documents/python/data/arctic_warming/cmip5/cmip5_aa.csv',index_col=0)
+    aa_ratios=df.loc[year].values.transpose().squeeze()
+elif model=='MPI-GE':
+    ds = xr.open_dataset('/Users/rantanem/Documents/python/data/arctic_warming/data_pdf_plots_MPI-ESM_rcp45.nc')
+    aa_ratios=ds.aa.values[:,ind]
 elif model=='CanESM5':
-    cmip6 = pd.read_csv('/Users/rantanem/Documents/python/data/arctic_warming/cmip6_aa.csv', index_col=0)
-    canesm_col = [s for s in cmip6.columns if 'CanESM5'.rstrip() in s]
-    mpi_ratios_plot = cmip6[canesm_col].loc[year].values.squeeze().ravel()
-elif model=='MIROC-ES2L':
-    cmip6 = pd.read_csv('/Users/rantanem/Documents/python/data/arctic_warming/cmip6_aa.csv', index_col=0)
-    canesm_col = [s for s in cmip6.columns if 'MIROC-ES2L'.rstrip() in s]
-    mpi_ratios_plot = cmip6[canesm_col].loc[year].values.squeeze().ravel()
+    df = pd.read_csv('/Users/rantanem/Documents/python/data/arctic_warming/cmip6_aa.csv', index_col=0)
+    canesm_col = [s for s in df.columns if 'CanESM5'.rstrip() in s]
+    aa_ratios = df[canesm_col].loc[year].values.squeeze().ravel()
 else:
     print('Model not valid!')
+    sys.exit()
 
 
 
@@ -59,12 +67,12 @@ a = AA.loc[year].mean()
 
 distribution_list = []
 
-n_times = 100000
+
 if model=='CMIP6':
-    models_with_multiple_real = models[models.ssp245>1]
+    
     for i in np.arange(0,n_times):
     
-        # sample with replacement of N runs
+        # sample with replacement of N models
         N = random.randrange(2, np.shape(models)[0])
         sample = random.choices(list(models.Name.values), k=N)
         oneruns = []
@@ -76,22 +84,24 @@ if model=='CMIP6':
         b = np.mean(oneruns)
             
     
-        # single run
-        
+        # single model
         single_model_i = random.randrange(0, np.shape(models_with_multiple_real)[0])
         single_model = models_with_multiple_real.iloc[single_model_i]
         matching = [s for s in all_realizations if single_model.Name.rstrip() in s]
         n = len(matching)
         
+        # single member from the single model
         random_run_i = random.randrange(0, len(matching))
         single_run_name = matching[random_run_i]
         single_run = aa_ratios[single_run_name].values
         
+        # mean of the AA ratios of that model's ensemble
         ensemble_mean = aa_ratios[matching].mean(axis=1).values
         
+        # the difference of AA between the single member and the mean
         c = single_run - ensemble_mean
         
-   
+        # to compensate the loss of variance due to small size of the model's ensemble, multiply by the factor
         multiplication_factor = (n/(n-1))**0.5
     
         c = c*multiplication_factor
@@ -101,17 +111,18 @@ else:
     for i in np.arange(0,n_times):
     
         # sample of N runs with replacement
-        N = random.randrange(2, np.shape(mpi_ratios_plot)[0])
-        indices = random.choices(np.arange(0,len(mpi_ratios_plot)), k=N)
-        sample = mpi_ratios_plot[indices]
+        N = random.randrange(2, np.shape(aa_ratios)[0])
+        indices = random.choices(np.arange(0,len(aa_ratios)), k=N)
+        sample = aa_ratios[indices]
 
         b = np.mean(sample)
     
         # single run
-        jth_member = random.randrange(0, np.shape(mpi_ratios_plot)[0])
-        single_run = np.squeeze(mpi_ratios_plot[jth_member])
+        jth_member = random.randrange(0, np.shape(aa_ratios)[0])
+        single_run = np.squeeze(aa_ratios[jth_member])
         c = b - single_run
     
+        # to compensate the loss of variance due to small size of the model's ensemble, multiply by the factor
         multiplication_factor = (N/(N-1))**0.5
     
         c = c*multiplication_factor
@@ -120,9 +131,10 @@ else:
 
 distribution = np.array(distribution_list)
     
+# plot the distribution
 fig, ax= plt.subplots(nrows=1, ncols=1, figsize=(5,5), dpi=200, sharex=False)
 plt.hist(distribution, bins=np.arange(-3,3,0.1), facecolor='darkgrey', edgecolor='k')
 
-
+#print the p-value
 cond = np.sum(distribution < 0)
 print('P-value: '+str(np.sum(distribution < 0)/n_times))
