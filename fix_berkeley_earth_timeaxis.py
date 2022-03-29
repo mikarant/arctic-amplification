93bd6bc01f2d9d2daeacbb67c390e42cfddde8e9
mikarant <mika.rantanen@fmi.fi>
2022-03-29 11:13:10 +0300

    revised version

--- fix_berkeley_earth_timeaxis.py
+++ fix_berkeley_earth_timeaxis.py
@@ -10,15 +10,15 @@ import pandas as pd
 import xarray as xr
 import numpy as np
 
-inputpath = '/home/rantanem/Documents/python/data/arctic_warming/GISTEMP-regridded.nc'
-outputpath = '/home/rantanem/Documents/python/data/arctic_warming/GISTEMP-regridded-retimed.nc'
+inputpath = '/Users/rantanem/Documents/python/data/arctic_warming/BEST.nc'
+outputpath = '/Users/rantanem/Documents/python/data/arctic_warming/BEST-retimed.nc'
 
 # Open the file as a dataset
 ds = xr.open_dataset(inputpath)
 
 
 # Create a new time axis which better follows the netCDF standards
-years = np.arange(1850,2021)
+years = np.arange(1850,2022)
 
 t_axis = pd.date_range(str(years[0])+'-01-01',pd.Timestamp(str(years[-1])+'-12-31'), freq='1M')
 
@@ -26,4 +26,4 @@ t_axis = pd.date_range(str(years[0])+'-01-01',pd.Timestamp(str(years[-1])+'-12-3
 # Change the time variable inside the dataset and rewrite to a temp file
 
 ds['time'] = t_axis
-ds.to_netcdf(outputpath)
+ds.to_netcdf(outputpath, format='NETCDF4', encoding={'time': {'dtype': 'i4'}})
