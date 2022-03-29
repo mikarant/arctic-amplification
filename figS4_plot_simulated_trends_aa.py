273fbf9110ebd42d68da8cb1d8333c757a51d33a
mikarant <mika.rantanen@fmi.fi>
2022-03-29 08:28:12 +0300

    rename

--- figS4_plot_simulated_trends_aa.py
+++ figS4_plot_simulated_trends_aa.py
@@ -10,6 +10,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import xarray as xr
 
+#ending year of the trend and AA
+year=2021
+
 
 # ## annotations
 annot=False
@@ -31,9 +34,9 @@ elinewidth = 1
 capthick=1
 
 ### read cmip6 results
-cmip6_ref_trends = pd.read_csv('/Users/rantanem/Documents/python/data/arctic_warming/cmip6_global_trends.csv',index_col=0)
-cmip6_arctic_trends = pd.read_csv('/Users/rantanem/Documents/python/data/arctic_warming/cmip6_arctic_trends.csv',index_col=0)
-cmip6_ratios = pd.read_csv('/Users/rantanem/Documents/python/data/arctic_warming/cmip6_aa.csv', index_col=0)
+cmip6_ref_trends = pd.read_csv('/Users/rantanem/Documents/python/data/arctic_warming/cmip6/cmip6_global_trends_ann.csv',index_col=0)
+cmip6_arctic_trends = pd.read_csv('/Users/rantanem/Documents/python/data/arctic_warming/cmip6/cmip6_arctic_trends_ann.csv',index_col=0)
+cmip6_ratios = pd.read_csv('/Users/rantanem/Documents/python/data/arctic_warming/cmip6/cmip6_aa_ann.csv', index_col=0)
 
 
 ### read cmip5 results
@@ -42,9 +45,9 @@ cmip5_arctic_trends = pd.read_csv('/Users/rantanem/Documents/python/data/arctic_
 cmip5_ratios = pd.read_csv('/Users/rantanem/Documents/python/data/arctic_warming/cmip5/cmip5_aa.csv',index_col=0)
 
 ## read mpi results
-mpi_ds = xr.open_dataset('/Users/rantanem/Documents/python/data/arctic_warming/data_pdf_plots_MPI-ESM_rcp45.nc')
-mpi_ind_2019 = np.isin(np.arange(1889,2100), 2019)
-mpi_ind_2018 = np.isin(np.arange(1889,2100), 2018)
+mpi_ref_trends = pd.read_csv('/Users/rantanem/Documents/python/data/arctic_warming/mpi-ge_global_trends_ann.csv',index_col=0)
+mpi_arctic_trends = pd.read_csv('/Users/rantanem/Documents/python/data/arctic_warming/mpi-ge_arctic_trends_ann.csv',index_col=0)
+mpi_ratios = pd.read_csv('/Users/rantanem/Documents/python/data/arctic_warming/mpi-ge_aa_ann.csv',index_col=0)
 
 ## read canesm5 results
 canesm_col = [s for s in cmip6_ref_trends.columns if 'CanESM5'.rstrip() in s]
@@ -80,9 +83,9 @@ fig, axlist= plt.subplots(nrows=1, ncols=2, figsize=(10,4), dpi=200, sharex=Fals
 
 ######### CMIP5 ################
 # plot cmip5 for arctic
-y = cmip5_arctic_trends.loc[2019].mean(axis=0)*10
-ymin = y - cmip5_arctic_trends.quantile(range_min, axis=1)[2019]*10
-ymax =  cmip5_arctic_trends.quantile(range_max, axis=1)[2019]*10 - y
+y = cmip5_arctic_trends.loc[year].mean(axis=0)*10
+ymin = y - cmip5_arctic_trends.quantile(range_min, axis=1)[year]*10
+ymax =  cmip5_arctic_trends.quantile(range_max, axis=1)[year]*10 - y
 yerr = [[ymin],[ymax]]
 
 axlist[0].errorbar(0.93,y, yerr=yerr,
@@ -90,8 +93,8 @@ axlist[0].errorbar(0.93,y, yerr=yerr,
 
 ## plot cmip5 for global
 y = cmip5_ref_trends.loc[2019].mean(axis=0)*10
-ymin = y - cmip5_ref_trends.quantile(range_min, axis=1)[2019]*10
-ymax =  cmip5_ref_trends.quantile(range_max, axis=1)[2019]*10 - y
+ymin = y - cmip5_ref_trends.quantile(range_min, axis=1)[year]*10
+ymax =  cmip5_ref_trends.quantile(range_max, axis=1)[year]*10 - y
 yerr = [[ymin],[ymax]]
 
 axlist[0].errorbar(1.07, y, yerr=yerr,
@@ -102,18 +105,18 @@ axlist[0].errorbar(1.07, y, yerr=yerr,
 
 ######### CMIP6 ################
 ## plot cmip6 for arctic
-y = cmip6_arctic_trends.loc[2019].mean(axis=0)*10
-ymin = y - cmip6_arctic_trends.quantile(range_min, axis=1)[2019]*10
-ymax =  cmip6_arctic_trends.quantile(range_max, axis=1)[2019]*10 - y
+y = cmip6_arctic_trends.loc[year].mean(axis=0)*10
+ymin = y - cmip6_arctic_trends.quantile(range_min, axis=1)[year]*10
+ymax =  cmip6_arctic_trends.quantile(range_max, axis=1)[year]*10 - y
 yerr = [[ymin],[ymax]]
 
 axlist[0].errorbar(1.93,y, yerr=yerr,
                     fmt='o', capsize=5, capthick=2, elinewidth=elinewidth, c=cmap(0))
 
 ## plot cmip6 for global
-y = cmip6_ref_trends.loc[2019].mean(axis=0)*10
-ymin = y - cmip6_ref_trends.quantile(range_min, axis=1)[2019]*10
-ymax =  cmip6_ref_trends.quantile(range_max, axis=1)[2019]*10 - y
+y = cmip6_ref_trends.loc[year].mean(axis=0)*10
+ymin = y - cmip6_ref_trends.quantile(range_min, axis=1)[year]*10
+ymax =  cmip6_ref_trends.quantile(range_max, axis=1)[year]*10 - y
 yerr = [[ymin],[ymax]]
 
 axlist[0].errorbar(2.07, y, yerr=yerr,
@@ -123,18 +126,18 @@ axlist[0].errorbar(2.07, y, yerr=yerr,
 
 ######### MPI_GE ################
 ## plot mpi-ge for arctic
-y =  mpi_ds['arctic_trend'].values[:,mpi_ind_2019].mean(axis=0).squeeze()
-ymin = y - mpi_ds['arctic_trend'][:,mpi_ind_2019].squeeze().quantile(range_min).values
-ymax =  mpi_ds['arctic_trend'][:,mpi_ind_2019].squeeze().quantile(range_max,).values - y
+y = mpi_arctic_trends.loc[year].mean(axis=0)*10
+ymin = y - mpi_arctic_trends.loc[year].quantile(range_min)*10
+ymax =  mpi_arctic_trends.loc[year].quantile(range_max)*10 - y
 yerr = [[ymin],[ymax]]
 
 axlist[0].errorbar(2.93,y, yerr=yerr,
                     fmt='o', capsize=5, capthick=2, elinewidth=elinewidth, c=cmap(0))
 
  ## plot mpi-ge for global
-y = mpi_ds['global_trend'].values[:,mpi_ind_2019].mean(axis=0).squeeze()
-ymin = y - mpi_ds['global_trend'][:,mpi_ind_2019].squeeze().quantile(range_min).values
-ymax =  mpi_ds['global_trend'][:,mpi_ind_2019].squeeze().quantile(range_max,).values - y
+y = mpi_ref_trends.loc[year].mean(axis=0)*10
+ymin = y - mpi_ref_trends.loc[year].quantile(range_min)*10
+ymax =  mpi_ref_trends.loc[year].quantile(range_max)*10 - y
 yerr = [[ymin],[ymax]]
 
 axlist[0].errorbar(3.07,y, yerr=yerr,
@@ -144,18 +147,18 @@ axlist[0].errorbar(3.07,y, yerr=yerr,
 
 ######### CanESM5 ################
 ## plot canesm5 for arctic
-y = canesm_arctic_trends.loc[2019].mean(axis=0)*10
-ymin = y - canesm_arctic_trends.quantile(range_min, axis=1)[2019]*10
-ymax =  canesm_arctic_trends.quantile(range_max, axis=1)[2019]*10 - y
+y = canesm_arctic_trends.loc[year].mean(axis=0)*10
+ymin = y - canesm_arctic_trends.quantile(range_min, axis=1)[year]*10
+ymax =  canesm_arctic_trends.quantile(range_max, axis=1)[year]*10 - y
 yerr = [[ymin],[ymax]]
 
 axlist[0].errorbar(3.93,y, yerr=yerr,
                     fmt='o', capsize=5, capthick=2, elinewidth=elinewidth, c=cmap(0))
 
 ## plot canesm5 for global
-y = canesm_ref_trends.loc[2019].mean(axis=0)*10
-ymin = y - canesm_ref_trends.quantile(range_min, axis=1)[2019]*10
-ymax =  canesm_ref_trends.quantile(range_max, axis=1)[2019]*10 - y
+y = canesm_ref_trends.loc[year].mean(axis=0)*10
+ymin = y - canesm_ref_trends.quantile(range_min, axis=1)[year]*10
+ymax =  canesm_ref_trends.quantile(range_max, axis=1)[year]*10 - y
 yerr = [[ymin],[ymax]]
 
 axlist[0].errorbar(4.07, y, yerr=yerr,
@@ -195,97 +198,68 @@ axlist[0].set_ylim(0.1,1.72)
 
 ######### CMIP5 ################
 ## plot cmip5
-y = cmip5_ratios.loc[2019].mean()
-ymin = y - cmip5_ratios.quantile(range_min, axis=1)[2019]
-ymax =  cmip5_ratios.quantile(range_max, axis=1)[2019] - y
+y = cmip5_ratios.loc[year].mean()
+ymin = y - cmip5_ratios.quantile(range_min, axis=1)[year]
+ymax =  cmip5_ratios.quantile(range_max, axis=1)[year] - y
 yerr = [[ymin],[ymax]]
 
 
 axlist[1].errorbar(1, y, yerr=yerr,
                     fmt='o', capsize=5, capthick=2, elinewidth=elinewidth, c=cmap(0))
 
-y = cmip5_ratios.loc[2018].mean()
-ymin = y - cmip5_ratios.quantile(range_min, axis=1)[2018]
-ymax =  cmip5_ratios.quantile(range_max, axis=1)[2018] - y
-yerr = [[ymin],[ymax]]
-
-
-# axlist[1].errorbar(1.07, y, yerr=yerr,
-#                     fmt='o', capsize=5, capthick=2, elinewidth=elinewidth, c=cmap(1))
 
 
 ######### CMIP6 ################
 ## plot cmip6
-y = cmip6_ratios.loc[2019].mean()
-ymin = y - cmip6_ratios.quantile(range_min, axis=1)[2019]
-ymax =  cmip6_ratios.quantile(range_max, axis=1)[2019] - y
+y = cmip6_ratios.loc[year].mean()
+ymin = y - cmip6_ratios.quantile(range_min, axis=1)[year]
+ymax =  cmip6_ratios.quantile(range_max, axis=1)[year] - y
 yerr = [[ymin],[ymax]]
 
 axlist[1].errorbar(2, y, yerr=yerr,
                     fmt='o', capsize=5, capthick=2, elinewidth=elinewidth, c=cmap(0))
 
-y = cmip6_ratios.loc[2018].mean()
-ymin = y - cmip6_ratios.quantile(range_min, axis=1)[2018]
-ymax =  cmip6_ratios.quantile(range_max, axis=1)[2018] - y
-yerr = [[ymin],[ymax]]
 
-# axlist[1].errorbar(2.07, y, yerr=yerr,
-#                     fmt='o', capsize=5, capthick=2, elinewidth=elinewidth, c=cmap(1))
 
 ######### MPI_GE ################
 
 ## plot mpi mean & maximum
-y = mpi_ds['aa'].values[:,mpi_ind_2019].mean().squeeze()
-ymin = y - mpi_ds['aa'][:,mpi_ind_2019].squeeze().quantile(range_min).values
-ymax =  mpi_ds['aa'][:,mpi_ind_2019].squeeze().quantile(range_max,).values - y
+y = mpi_ratios.loc[year].mean()
+ymin = y - mpi_ratios.quantile(range_min, axis=1)[year]
+ymax =  mpi_ratios.quantile(range_max, axis=1)[year] - y
 yerr = [[ymin],[ymax]]
 
 axlist[1].errorbar(3, y, yerr=yerr,
                     fmt='o', capsize=5, capthick=2, elinewidth=elinewidth, c=cmap(0))
 
-y = mpi_ds['aa'].values[:,mpi_ind_2018].mean().squeeze()
-ymin = y - mpi_ds['aa'][:,mpi_ind_2018].squeeze().quantile(range_min).values
-ymax =  mpi_ds['aa'][:,mpi_ind_2018].squeeze().quantile(range_max,).values - y
-yerr = [[ymin],[ymax]]
-
-# axlist[1].errorbar(3.07, y, yerr=yerr,
-#                     fmt='o', capsize=5, capthick=2, elinewidth=elinewidth, c=cmap(1))
-
 
 ######### CanESM5 ################
 ## plot canesm
-y = canesm_ratios.loc[2019].mean()
-ymin = y - canesm_ratios.quantile(range_min, axis=1)[2019]
-ymax =  canesm_ratios.quantile(range_max, axis=1)[2019] - y
+y = canesm_ratios.loc[year].mean()
+ymin = y - canesm_ratios.quantile(range_min, axis=1)[year]
+ymax =  canesm_ratios.quantile(range_max, axis=1)[year] - y
 yerr = [[ymin],[ymax]]
 
 axlist[1].errorbar(4, y, yerr=yerr,
                     fmt='o', capsize=5, capthick=2, elinewidth=elinewidth, c=cmap(0))
 
-y = canesm_ratios.loc[2018].mean()
-ymin = y - canesm_ratios.quantile(range_min, axis=1)[2018]
-ymax =  canesm_ratios.quantile(range_max, axis=1)[2018] - y
-yerr = [[ymin],[ymax]]
-
-# axlist[1].errorbar(4.07, y, yerr=yerr,
-#                     fmt='o', capsize=5, capthick=2, elinewidth=elinewidth, c=cmap(1))
 
 
 
 # observed AA with line
-axlist[1].axhline(y=df_obs.loc[2019].mean(), 
+axlist[1].axhline(y=df_obs.loc[year].mean(), 
                   c=cmap(0), linestyle='--', linewidth=1)
 
 
 axlist[1].grid(axis='y')
 axlist[1].set_ylabel('Arctic amplification', fontsize=14)
 
-labels = ['AA 1980-2019']
+labels = ['AA 1979-2021']
 axlist[1].legend(handles[:], labels[:], fontsize=12, ncol=2, bbox_to_anchor=(1.02, 1.16))
 
 plt.xticks(xticks,labels=longnames, fontsize=12)
 plt.yticks(np.arange(1.,5,0.5), fontsize=14)
-axlist[1].set_ylim(1.5,4)
+axlist[1].set_ylim(1.4,4)
 
 # increase width between the plots
 plt.subplots_adjust(wspace=0.4)
